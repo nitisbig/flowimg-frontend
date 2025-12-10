06e1673aa088981de71142c5a93e5050d15d7da2
Nitesh Ghimire <niteshghimire223@gmail.com>
2025-12-10 18:05:45 +0545

    change x-key to x-api-key

--- lib/api.ts
+++ lib/api.ts
@@ -5,7 +5,7 @@ const api_key = process.env.NEXT_PUBLIC_MY_API
 const api = axios.create({
     baseURL: 'https://api.flowdira.com',
     headers: {
-        "x-key": api_key
+        "x-api-key": api_key
     }
 })
 
